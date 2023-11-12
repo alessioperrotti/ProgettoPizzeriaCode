73f9687b33936dad19c9d2aec7434b9a76252a35
andreaperugini <andreaperugini74@gmail.com>
2023-11-12 16:32:59 +0100

    Riorganizzazione + Clean del codice

--- Code/Gestione Sistema/View/VistaHomeAdmin.py
+++ Code/Gestione Sistema/View/VistaHomeAdmin.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python
 
 import sys
+
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (QWidget, QLCDNumber, QSlider, QLabel, QPushButton, QVBoxLayout, QApplication, QSpacerItem,
-                             QSizePolicy, QHBoxLayout, QFrame, QLineEdit, QGridLayout, QTableWidget, QTableWidgetItem,
-                             QHeaderView)
 from PyQt6.QtGui import QFont, QPixmap
+from PyQt6.QtWidgets import (QWidget, QLabel, QPushButton, QVBoxLayout, QApplication, QSizePolicy, QHBoxLayout,
+                             QGridLayout, QTableWidget, QHeaderView)
 
 label_font = QFont("Roboto", 24)
 label_font_tit = QFont("Roboto", 32, weight=50)
 label_font_piccolo = QFont("Roboto", 14)
 
-def Pulsante(nome, directory):
+
+def crea_pulsante(nome, directory):
     pulsante = QPushButton()
     label = QLabel(nome)
     label.setFont(label_font_piccolo)
     layout = QHBoxLayout()
     layout.addStretch()
-    layout.addWidget(Immagine(directory, 50))
+    layout.addWidget(crea_immagine(directory, 50))
     layout.addWidget(label)
     layout.addStretch()
     pulsante.setLayout(layout)
-    pulsante.setSizePolicy(QSizePolicy.Policy.Minimum,QSizePolicy.Policy.Minimum)
-    pulsante.setStyleSheet(".QPushButton {background-color: #ff776d; border: 2px solid black; border-radius: 10px; padding: 10px}")
-    pulsante.setFixedSize(230,100)
+    pulsante.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+    pulsante.setStyleSheet(
+        ".QPushButton {background-color: #ff776d; border: 2px solid black; border-radius: 10px; padding: 10px}")
+    pulsante.setFixedSize(230, 100)
     return pulsante
 
-def Immagine(directory, dimensione):
+
+def crea_immagine(directory, dimensione):
     label_foto = QLabel()
     pixmap = QPixmap(directory)
     scaled_pixmap = pixmap.scaled(dimensione, dimensione, aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio)
@@ -34,7 +37,7 @@ def Immagine(directory, dimensione):
     return label_foto
 
 
-def Tabella(nColonne, larghezza, altezza):
+def crea_tabella(n_colonne, larghezza, altezza):
     tabella = QTableWidget()
     tabella.setStyleSheet("""
                QTableWidget {
@@ -54,12 +57,12 @@ def Tabella(nColonne, larghezza, altezza):
                }
            """)
 
-    tabella.setColumnCount(nColonne)
+    tabella.setColumnCount(n_colonne)
 
     header = tabella.horizontalHeader()
     header.setFont(label_font_piccolo)
     header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
-    for i in range(1, nColonne):
+    for i in range(1, n_colonne):
         header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
     tabella.setFixedWidth(larghezza)
     tabella.setFixedHeight(altezza)
@@ -67,45 +70,43 @@ def Tabella(nColonne, larghezza, altezza):
     return tabella
 
 
-
-
-class Vista_Home_Admin(QWidget):
+class VistaHomeAdmin(QWidget):
     def __init__(self):
         super().__init__()
         self.initUi()
 
     def initUi(self):
-        #definizione oggetti
+        # definizione oggetti
         label = QLabel("ACCESSO: Admin")
         label.setFont(label_font_tit)
-        pulsante1=Pulsante("Statistiche", "png/stats.png");
-        pulsante2=Pulsante("Magazzino",  "png/magazzino.png");
-        pulsante3=Pulsante("Dipendenti", "png/dipendenti.png");
-        pulsante4=Pulsante("Turni", "png/turni.png");
-        pulsante5=Pulsante("Modifica Menu", "png/stats.png");
-        pulsante6=Pulsante("Prenotazioni", "png/stats.png");
-        pulsante7=Pulsante("Ricevute", "png/stats.png");
+        pulsante1 = crea_pulsante("Statistiche", "png/stats.png")
+        pulsante2 = crea_pulsante("Magazzino", "png/magazzino.png")
+        pulsante3 = crea_pulsante("Dipendenti", "png/dipendenti.png")
+        pulsante4 = crea_pulsante("Turni", "png/turni.png")
+        pulsante5 = crea_pulsante("Modifica Menu", "png/stats.png")
+        pulsante6 = crea_pulsante("Prenotazioni", "png/stats.png")
+        pulsante7 = crea_pulsante("Ricevute", "png/stats.png")
 
-        back = Immagine("png/back.png",35)
+        back = crea_immagine("png/back.png", 35)
 
-        #definizione layout
+        # definizione layout
         layout = QVBoxLayout()
         griglia = QGridLayout()
         layout_centrale = QHBoxLayout()
         layout_orizzontale = QHBoxLayout()
 
-        #inserimento oggetti nei layout
+        # inserimento oggetti nei layout
         layout_centrale.addStretch()
         layout_centrale.addLayout(griglia)
         layout_centrale.addStretch()
 
-        griglia.addWidget(pulsante1, 1,1)
-        griglia.addWidget(pulsante2, 1,2)
-        griglia.addWidget(pulsante3, 2,1)
-        griglia.addWidget(pulsante4, 2,2)
-        griglia.addWidget(pulsante5, 3,1)
-        griglia.addWidget(pulsante6, 3,2)
-        griglia.addWidget(pulsante7, 4,1,1,2, alignment=Qt.AlignmentFlag.AlignCenter)
+        griglia.addWidget(pulsante1, 1, 1)
+        griglia.addWidget(pulsante2, 1, 2)
+        griglia.addWidget(pulsante3, 2, 1)
+        griglia.addWidget(pulsante4, 2, 2)
+        griglia.addWidget(pulsante5, 3, 1)
+        griglia.addWidget(pulsante6, 3, 2)
+        griglia.addWidget(pulsante7, 4, 1, 1, 2, alignment=Qt.AlignmentFlag.AlignCenter)
 
         layout.addSpacing(10)
         layout.addWidget(label)
@@ -119,26 +120,16 @@ class Vista_Home_Admin(QWidget):
         layout_orizzontale.addLayout(layout)
         layout_orizzontale.addSpacing(20)
 
-
         self.setFixedSize(994, 637)
         self.setLayout(layout_orizzontale)
         self.show()
 
 
-
-
-
-
-
-
-
 def main():
     app = QApplication(sys.argv)
-    ex = Vista_Home_Admin()
+    ex = VistaHomeAdmin()
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
     main()
-
-
