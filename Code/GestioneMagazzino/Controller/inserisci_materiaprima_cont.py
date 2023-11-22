7c5785d1b76cc87e78a2324f2508da35c486a8ed
Alessio Perrotti <perrottialessio@gmail.com>
2023-11-23 00:01:38 +0100

    Update

--- Code/GestioneMagazzino/Controller/inserisci_materiaprima_cont.py
+++ Code/GestioneMagazzino/Controller/inserisci_materiaprima_cont.py
@@ -1,6 +1,6 @@
 from Code.GestioneMagazzino.View.inserisci_materiaprima_view import VistaInserisciMateriaPrima
 from Code.GestioneMagazzino.Model.materia_prima import MateriaPrima
-
+from PyQt6.QtWidgets import QMessageBox
 
 class ContInserisciMateriaPrima(object):
 
@@ -15,11 +15,32 @@ class ContInserisciMateriaPrima(object):
 
         codice = self.view.campo_codice.text()
         nome = self.view.campo_nome.text()
-        costo_al_kg = self.view.campo_costoAlKg.text()
-        qta_disponibile = self.view.campo_qtaDisp.text()
-        data_scadenza = self.view.campo_dataScadenza.selectedDate()
-        qta_limite = self.view.campo_qtaLimite.text()
-        qta_ordine_STD = self.view.campo_qtaOrdineSTD.text()
+
+        #prima c'era solo il blocco tra try e break
+        while True:
+            try:
+                costo_al_kg = round(float(self.view.campo_costoAlKg.text()), 2)
+                qta_disponibile = round(float(self.view.campo_qtaDisp.text()), 3)
+                data_scadenza = self.view.campo_dataScadenza.selectedDate()
+                qta_limite = round(float(self.view.campo_qtaLimite.text()), 3)
+                qta_ordine_STD = round(float(self.view.campo_qtaOrdineSTD.text()), 3)
+                break
+            except ValueError as e:
+                if not all([
+                    self.view.campo_costoAlKg.text(),
+                    self.view.campo_qtaDisp.text(),
+                    self.view.campo_qtaLimite.text(),
+                    self.view.campo_qtaOrdineSTD.text(),
+                ]):
+                    errore_msg = "Controllare che tutti i campi siano riempiti."
+                else:
+                    errore_msg = "Controllare che i dati siano inseriti correttamente."
+
+                error_box = QMessageBox()
+                error_box.setIcon(QMessageBox.Icon.Critical)
+                error_box.setWindowTitle("Errore di Inserimento")
+                error_box.setText(errore_msg)
+                error_box.exec()
 
         nuova_materia_prima = MateriaPrima(codice, nome, costo_al_kg, qta_disponibile,
                                            qta_limite, qta_ordine_STD, data_scadenza)
