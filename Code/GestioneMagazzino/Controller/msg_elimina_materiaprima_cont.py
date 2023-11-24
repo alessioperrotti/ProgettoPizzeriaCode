15395f68a82d3211edee3b45f4697b3fde1dfbcf
Alessio Perrotti <perrottialessio@gmail.com>
2023-11-24 12:49:17 +0100

    Update
    Gestione magazzino finita

--- Code/GestioneMagazzino/Controller/msg_elimina_materiaprima_cont.py
+++ Code/GestioneMagazzino/Controller/msg_elimina_materiaprima_cont.py
@@ -3,7 +3,16 @@ from Code.GestioneMagazzino.View.msg_elimina_materiaprima_view import VistaMsgEl
 class ContMsgEliminaMateriaPrima(object):
     def __init__(self, view: VistaMsgEliminaMateriaPrima):
         self.view = view
+        self.conferma = False
         self.view.pulsante2.clicked.connect(self.confermato)
+        self.view.pulsante1.clicked.connect(self.annullato)
 
     def confermato(self):
-        return True
+        print("confermato")
+        self.conferma = True
+        self.view.close()
+
+    def annullato(self):
+        print("annullato")
+        self.conferma = False
+        self.view.close()
