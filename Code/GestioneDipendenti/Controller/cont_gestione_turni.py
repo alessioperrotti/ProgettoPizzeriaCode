270f9af89c8dd5536a8cc3fb3dec662d77482b06
RiccardoZag <riccardozagaglia02@gmail.com>
2023-11-30 12:55:52 +0100

    Funziona tutto gg

--- Code/GestioneDipendenti/Controller/cont_gestione_turni.py
+++ Code/GestioneDipendenti/Controller/cont_gestione_turni.py
@@ -41,8 +41,8 @@ class ContGestioneTurni(object):
         self.update_tabella()
 
     def update_tabella(self):
-
         self.view.tabella.clearContents()
+
         #stampa di verifica
         for cuoco in self.model.lista_cuochi:
             for i in range(len(cuoco.turno)):
@@ -52,89 +52,73 @@ class ContGestioneTurni(object):
             for i in range(len(cameriere.turno)):
                 print("indice_giorno = " + str(i) + " " + str(cameriere.nome) + " turno:" + str(cameriere.turno[i])+"\n")
 
-        # Crea una mappatura per i tipi di turno agli indici di riga
-        # mappatura_turni = {"Pranzo": 0, "Cena": 1, "Pranzo & Cena": [0, 1]}
-        #
-        # for dipendente in self.model.lista_cuochi + self.model.lista_camerieri:
-        #     for i, turno in enumerate(dipendente.turno):
-        #         item_vuoto = QTableWidgetItem("vuoto")
-        #         item_vuoto.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        #         item_nome = QTableWidgetItem(dipendente.nome + " " + dipendente.cognome)
-        #         item_nome.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        #
-        #         indice_riga = mappatura_turni.get(turno, None)
-        #
-        #         if indice_riga is not None:
-        #             if isinstance(indice_riga, list):
-        #                 for idx in indice_riga:
-        #                     self.view.tabella.setItem(idx, i, item_nome)
-        #             else:
-        #                 self.view.tabella.setItem(indice_riga, i, item_nome)
-        #         elif turno is None:
-        #             self.view.tabella.setItem(0, i, item_vuoto)
-        #             item_vuoto_corrente = QTableWidgetItem(item_vuoto.text())
-        #             item_vuoto_corrente.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        #             self.view.tabella.setItem(1, i, item_vuoto_corrente)
-        #         else:
-        #             for k in range(self.view.tabella.rowCount()):
-        #                 for j in range(self.view.tabella.columnCount()):
-        #                     item_vuoto = QTableWidgetItem("vuoto")
-        #                     item_vuoto.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        #                     self.view.tabella.setItem(k, j, item_vuoto)
-
-
-        # for row in range(self.view.tabella.rowCount()):
-        # for col in range(self.view.tabella.columnCount()):
-        #     item = QTableWidgetItem("vuoto")
-        #     self.view.tabella.setItem(0, col, item)
-        #     item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-
         for cuoco in self.model.lista_cuochi:
-            #print(len(cuoco.turno))
             for i in range(len(cuoco.turno)):
-                item_vuoto = QTableWidgetItem("vuoto")
-                item_vuoto.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
                 item_nome = QTableWidgetItem(cuoco.nome + " " + cuoco.cognome)
                 item_nome.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
 
-
                 if cuoco.turno[i] == "Pranzo":
-                    self.view.tabella.setItem(0, i, item_nome)
+                    current_item = self.view.tabella.item(0, i)
+                    if current_item is not None:
+                        current_item.setText(f"{current_item.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(0, i, item_nome)
                 elif cuoco.turno[i] == "Cena":
-                    print("porco")
-                    self.view.tabella.setItem(1, i, item_nome)
-                    print("cazzo")
+                    current_item = self.view.tabella.item(1, i)
+                    if current_item is not None:
+                        current_item.setText(f"{current_item.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(1, i, item_nome)
                 elif cuoco.turno[i] == "Pranzo & Cena":
-                    self.view.tabella.setItem(0, i, item_nome)
-                    current_item_nome_cena = QTableWidgetItem(item_nome.text())
-                    current_item_nome_cena.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                    self.view.tabella.setItem(1, i, current_item_nome_cena)
-                # elif cuoco.turno[i] is None:
-                #     self.view.tabella.setItem(0, i, item_vuoto)
-                #     current_item_vuoto = QTableWidgetItem(item_vuoto.text())
-                #     current_item_vuoto.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                #     self.view.tabella.setItem(1, i, current_item_vuoto)
+                    # Handling "Pranzo & Cena" in both rows
+                    current_item_pranzo = self.view.tabella.item(0, i)
+                    current_item_cena = self.view.tabella.item(1, i)
+
+                    if current_item_pranzo is not None:
+                        current_item_pranzo.setText(f"{current_item_pranzo.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(0, i, item_nome)
+
+                    if current_item_cena is not None:
+                        current_item_cena.setText(f"{current_item_cena.text()}\n{item_nome.text()}")
+                    else:
+                        current_item_nome_cena = QTableWidgetItem(item_nome.text())
+                        current_item_nome_cena.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+                        self.view.tabella.setItem(1, i, current_item_nome_cena)
 
         for cameriere in self.model.lista_camerieri:
-            #print(len(cameriere.turno))
             for i in range(len(cameriere.turno)):
                 item_nome = QTableWidgetItem(cameriere.nome + " " + cameriere.cognome)
                 item_nome.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
 
-
                 if cameriere.turno[i] == "Pranzo":
-                    self.view.tabella.setItem(0, i, item_nome)
+                    current_item = self.view.tabella.item(0, i)
+                    if current_item is not None:
+                        current_item.setText(f"{current_item.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(0, i, item_nome)
                 elif cameriere.turno[i] == "Cena":
-                    self.view.tabella.setItem(1, i, item_nome)
+                    current_item = self.view.tabella.item(1, i)
+                    if current_item is not None:
+                        current_item.setText(f"{current_item.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(1, i, item_nome)
                 elif cameriere.turno[i] == "Pranzo & Cena":
-                    self.view.tabella.setItem(0, i, item_nome)
-                    current_item_nome_cena = QTableWidgetItem(item_nome.text())
-                    current_item_nome_cena.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                    self.view.tabella.setItem(1, i, current_item_nome_cena)
-                # elif cameriere.turno[i] is None:
-                #     self.view.tabella.setItem(0, i, item_vuoto)
-                #     current_item_vuoto = QTableWidgetItem(item_vuoto.text())
-                #     current_item_vuoto.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-                #     self.view.tabella.setItem(1, i, current_item_vuoto)
+                    # Handling "Pranzo & Cena" in both rows
+                    current_item_pranzo = self.view.tabella.item(0, i)
+                    current_item_cena = self.view.tabella.item(1, i)
+
+                    if current_item_pranzo is not None:
+                        current_item_pranzo.setText(f"{current_item_pranzo.text()}\n{item_nome.text()}")
+                    else:
+                        self.view.tabella.setItem(0, i, item_nome)
+
+                    if current_item_cena is not None:
+                        current_item_cena.setText(f"{current_item_cena.text()}\n{item_nome.text()}")
+                    else:
+                        current_item_nome_cena = QTableWidgetItem(item_nome.text())
+                        current_item_nome_cena.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+                        self.view.tabella.setItem(1, i, current_item_nome_cena)
+
 
 
