354c141eaf25924e9951bcc1842a36b3f523097a
RiccardoZag <riccardozagaglia02@gmail.com>
2023-12-04 23:12:54 +0100

    Update vista_piantina.py

--- Code/GestioneDipendenti/View/vista_piantina.py
+++ Code/GestioneDipendenti/View/vista_piantina.py
@@ -7,12 +7,31 @@ from PyQt6.QtWidgets import (QWidget, QLabel, QPushButton, QVBoxLayout, QApplica
                              QScrollBar, QScrollArea, QAbstractItemView, QDialog, QFrame)
 
 # Font
-label_font = QFont("Roboto", 24)
+label_font = QFont("Roboto", 12)
 label_font_tit = QFont("Roboto", 32, weight=50)
 label_font_piccolo = QFont("Roboto", 16)
 header_font = QFont("Roboto", 10)
 header_font.setBold(True)
 
+def crea_etichetta(testo, colore_pulsante):
+    layout_h = QHBoxLayout()
+
+    etichetta = QLabel(testo)
+    etichetta.setFont(label_font)
+
+    pulsante = QPushButton()
+    pulsante.setFixedSize(25, 25)
+    pulsante.setStyleSheet(f"""
+        QPushButton {{
+            background-color: {colore_pulsante};
+            border-radius: 2px;
+        }}
+    """)
+
+    layout_h.addWidget(pulsante)
+    layout_h.addWidget(etichetta)
+
+    return layout_h
 
 def crea_pulsante_back(dimensioni, directory):
     pulsante_back = QPushButton()
@@ -54,34 +73,32 @@ class VistaPiantina(QDialog):
                 background-color: "red";
                 font-size: 23px;
             }
-            QTableWidget {
-                background-color: white;
-                alternate-background-color: white;
-                selection-background-color: darkcyan;
-                border: 2px solid grey;
-            }
-            QHeaderView:section {
-                background-color: lightgray;
-                font-weight: bold;
-            }
-            QHeaderView:active {
-                background-color: gray;
-            }
         """)
 
     def init_ui(self):
         title = QLabel("Piantina")
         title.setFont(label_font_tit)
 
-        label_liberi = QLabel("LIBERI")
-        label_prenotati = QLabel("PRENOTATI")
-        label_occupati = QLabel("OCCUPATI")
-        label_inattesa = QLabel("IN ATTESA")
-        label_serviti = QLabel("SERVITI")
+        # label_liberi = QLabel("LIBERI")
+        # label_liberi.setFont(label_font)
+        # label_prenotati = QLabel("PRENOTATI")
+        # label_prenotati.setFont(label_font)
+        # label_occupati = QLabel("OCCUPATI")
+        # label_occupati.setFont(label_font)
+        # label_inattesa = QLabel("IN ATTESA")
+        # label_inattesa.setFont(label_font)
+        # label_serviti = QLabel("SERVITI")
+        # label_serviti.setFont(label_font)
 
-        linea = QFrame()
-        linea.setFrameShape(QFrame.Shape.VLine)
-        linea.setFixedHeight(637)
+        label_liberi = crea_etichetta("LIBERI","grey")
+        label_prenotati = crea_etichetta("PRENOTATI","#007fff")
+        label_occupati = crea_etichetta("OCCUPATI","red")
+        label_inattesa = crea_etichetta("IN ATTESA","orange")
+        label_serviti = crea_etichetta("SERVITI","yellow")
+
+        linea_v = QFrame()
+        linea_v.setFrameShape(QFrame.Shape.VLine)
+        linea_v.setFixedHeight(637)
 
         self.pulsante_back = crea_pulsante_back(35, "png/back.png")
         self.pulsante_consegna = QPushButton("Consegna")
@@ -93,90 +110,78 @@ class VistaPiantina(QDialog):
         self.label_time = QLabel(self)
         self.label_time.setFont(label_font_piccolo)
 
-        # self.tav1 = QPushButton("1")
-        # self.tav2 = QPushButton("2")
-        # self.tav3 = QPushButton("3")
-        # self.tav4 = QPushButton("4")
-        # self.tav5 = QPushButton("5")
-        # self.tav6 = QPushButton("6")
-        # self.tav7 = QPushButton("7")
-        # self.tav8 = QPushButton("8")
-        # self.tav9 = QPushButton("9")
-        # self.tav10 = QPushButton("10")
-        # self.tav11 = QPushButton("11")
-        # self.tav12 = QPushButton("12")
-        # self.tav13 = QPushButton("13")
-        # self.tav14 = QPushButton("14")
-        # self.tav15 = QPushButton("15")
-        # self.tav16 = QPushButton("16")
-        # self.tav17 = QPushButton("17")
-
         # Imposta il timer per aggiornare l'orario ogni secondo
         timer = QTimer(self)
         timer.timeout.connect(self.update_datetime)
         timer.start(1000)
 
+        clock = QLabel()
+        img = QPixmap("png/check.png").scaled(35, 35)
+        clock.setPixmap(img)
+
         layout = QVBoxLayout()
         layout_title = QHBoxLayout()
         layout_h = QHBoxLayout()
         layout_griglia = QGridLayout()
-        layout_griglia.setSpacing(20)
+        layout_griglia.setSpacing(10)
         layout_v_dx = QVBoxLayout()
 
         layout.addLayout(layout_title)
         layout_title.addWidget(title, alignment=Qt.AlignmentFlag.AlignTop)
         layout_title.addStretch()
-        layout_title.addWidget(self.label_date, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
-        layout_title.addWidget(self.label_time, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
-        # layout_title.addWidget(clock, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        layout_title.addWidget(self.label_date, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        layout_title.addWidget(self.label_time, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        layout_title.addWidget(clock, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
 
-        layout_h.addSpacing(0)
         layout_h.addLayout(layout_griglia)
-        layout_h.addWidget(linea, alignment=Qt.AlignmentFlag.AlignRight)
+        layout_h.addWidget(linea_v, alignment=Qt.AlignmentFlag.AlignHCenter)
         layout_h.addLayout(layout_v_dx)
 
-        layout_v_dx.addWidget(label_liberi)
-        layout_v_dx.addWidget(label_prenotati)
-        layout_v_dx.addWidget(label_occupati)
-        layout_v_dx.addWidget(label_inattesa)
-        layout_v_dx.addWidget(label_serviti)
+        layout_v_dx.addSpacing(15)
+        layout_v_dx.addLayout(label_liberi)
+        layout_v_dx.addLayout(label_prenotati)
+        layout_v_dx.addLayout(label_occupati)
+        layout_v_dx.addLayout(label_inattesa)
+        layout_v_dx.addLayout(label_serviti)
+        layout_v_dx.addSpacing(15)
         layout_v_dx.addWidget(self.pulsante_consegna)
         layout_v_dx.addSpacing(200)
         layout_v_dx.addWidget(self.pulsante_back, alignment=Qt.AlignmentFlag.AlignRight)
 
-        for row in range(4):
+        for row in range(5):
             for col in range(4):
-                tavolo_label = QLabel(f"Tavolo {row * 4 + col + 1}")
-                tavolo_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                tavolo_label.setFrameShape(QFrame.Shape.Box)  # Aggiunge un bordo per evidenziare la cliccabilità
-                tavolo_label.setStyleSheet("QLabel { background-color : lightblue; }")  # Imposta lo stile
-                tavolo_label.setCursor(Qt.CursorShape.PointingHandCursor)  # Cambia il cursore al puntatore della mano
-                tavolo_label.mousePressEvent = lambda event, nome=tavolo_label.text(): on_tavolo_clicked(nome)
-                layout_griglia.addWidget(tavolo_label, row, col)
+                tavolo_numero = row * 4 + col + 1
 
-                # Imposta larghezza e altezza uguali per fare in modo che le dimensioni siano quadrate
-                tavolo_label.setFixedSize(150, 100)
+                if tavolo_numero <= 4:
+                    size = (170, 70)
+                elif tavolo_numero <= 8:
+                    size = (125, 70)
+                elif tavolo_numero <= 12:
+                    size = (100, 70)
+                else:
+                    size = (70, 70)
 
-        # layout_griglia.addWidget(self.tav1, 0, 0,2,0)
-        # layout_griglia.addWidget(self.tav2, 0, 1,2,0)
-        # layout_griglia.addWidget(self.tav3, 0, 2,2,0)
-        # layout_griglia.addWidget(self.tav4, 0, 3,2,0)
-        # layout_griglia.addWidget(self.tav5, 1, 0)
-        # layout_griglia.addWidget(self.tav6, 1, 1)
-        # layout_griglia.addWidget(self.tav7, 1, 2)
-        # layout_griglia.addWidget(self.tav8, 1, 3)
-        # layout_griglia.addWidget(self.tav9, 2, 0)
-        # layout_griglia.addWidget(self.tav10, 2, 1)
-        # layout_griglia.addWidget(self.tav11, 2, 2)
-        # layout_griglia.addWidget(self.tav12, 2, 3)
-        # layout_griglia.addWidget(self.tav13, 3, 0)
-        # layout_griglia.addWidget(self.tav14, 3, 1)
-        # layout_griglia.addWidget(self.tav15, 3, 2)
-        # layout_griglia.addWidget(self.tav16, 3, 3)
-        # layout_griglia.addWidget(self.tav17, 4, 0)
+                if tavolo_numero <= 18:
+                    tavolo_button = QPushButton(f"{tavolo_numero}")
+                    tavolo_button.clicked.connect(lambda checked, nome=tavolo_button.text(): on_tavolo_clicked(nome))
+                    layout_griglia.addWidget(tavolo_button, row, col, alignment=Qt.AlignmentFlag.AlignCenter)
+                    tavolo_button.setFixedSize(*size)
+                    tavolo_button.setStyleSheet("""
+                                                QPushButton{
+                                                background-color: "grey";
+                                                color: "black";
+                                                text-align: center;
+                                                border-radius: 6px;
+                                                border: 3px solid lightgrey;
+                                                font-family:Roboto;
+                                                }
+                                                QPushButton:hover{
+                                                font-size: 15px;
+                                                font-weight: bold;}""")
 
         layout.addLayout(layout_h)
         layout.addStretch()
+        linea_v.raise_()
 
         self.setLayout(layout)
         self.setFixedSize(994, 637)
