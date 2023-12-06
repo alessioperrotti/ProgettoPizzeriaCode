f80efb7830d620cfc73aa524df09f42437d30c67
RiccardoZag <riccardozagaglia02@gmail.com>
2023-12-06 23:47:08 +0100

    Update piantina, sistemato file
    ora la piantina vede solo i tavoli prenotati con data odierna

--- Code/GestioneDipendenti/Controller/cont_piantina.py
+++ Code/GestioneDipendenti/Controller/cont_piantina.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QPushButton, QWidget, QStackedWidget
-
+from datetime import datetime
 from Code.GestioneDipendenti.View.vista_piantina import VistaPiantina
 from Code.GestionePrenotazioni.Model.gestore_prenotazioni import GestorePrenotazioni
 
@@ -14,44 +14,54 @@ class ContPiantina(object):
         self.view.pulsante_consegna.clicked.connect(self.cambia_colore)
         self.view.pulsante_consegna.clicked.connect(self.update_tabella)
         self.update_tabella()
-        self.model.gestore_ordini_tavolo.carica_da_file()
+        # self.model.gestore_ordini_tavolo.carica_da_file()
 
     def cambia_colore(self):
         self.tavolo_selezionato = self.model.ricerca_tavolo(self.view.n_tavolo)
         self.tavolo_selezionato.cambia_stato("servito")
 
-        self.model.salva_tavoli()
-        self.model.carica_tavoli()
+        self.model.salva_dati("lista_prenotazioni.pickle", "lista_tavoli.pickle")
+        # self.model.salva_tavoli()
+        # self.model.carica_tavoli()
 
         # print("tavolo: "+str(self.tavolo_selezionato.numero)+" posti: "+str(self.tavolo_selezionato.posti_disponibili))
         # print(self.tavolo_selezionato.stato)
 
-
     def update_tabella(self):
+        data_corrente = datetime.now().date()
+        # print(data_corrente)
+
         for tavolo in self.model.lista_tavoli:
             numero_tavolo = tavolo.numero
             stato_tavolo = tavolo.stato
             tavolo_button = self.view.nome_tavoli_map[numero_tavolo]
 
+            tavolo_prenotato = False
+            for prenotazione in self.model.lista_prenotazioni:
+                if int(prenotazione.tavolo_assegnato.numero) == int(numero_tavolo) and prenotazione.data.toPyDate() == data_corrente:
+                    # print("stessa data")
+                    tavolo_prenotato = True
+                    break
+
             if tavolo_button is not None:
-                if str(stato_tavolo) == "libero":
+                if tavolo_prenotato:
+                    # stato_tavolo == "prenotato":
                     tavolo_button.setStyleSheet("""
-                            QPushButton{
-                                background-color: "grey";
-                                color: "black";
-                                text-align: center;
-                                border-radius: 6px;
-                                border: 3px solid lightgrey;
-                                font-family: Roboto;
-                            }
-                            QPushButton:checked{
-                                                    background-color: "lightgrey";
-                                                    color: "black";
-                                                }
-                            QPushButton:hover{
-                                font-size: 15px;
-                                font-weight: bold;
-                            }""")
+                                                QPushButton{
+                                                    background-color: "#007fff";
+                                                    color: "white";
+                                                    text-align: center;
+                                                    border-radius: 6px;
+                                                    border: 3px solid lightgrey;
+                                                    font-family: Roboto;
+                                                }QPushButton:checked{
+                                                                        background-color: "lightblue";
+                                                                        color: "black";
+                                                                    }
+                                                QPushButton:hover{
+                                                    font-size: 15px;
+                                                    font-weight: bold;
+                                                }""")
                 elif stato_tavolo == "servito":
                     tavolo_button.setStyleSheet("""
                             QPushButton{
@@ -63,23 +73,6 @@ class ContPiantina(object):
                                 font-family: Roboto;
                             }QPushButton:checked{
                                                     background-color: "lightyellow";
-                                                    color: "black";
-                                                }
-                            QPushButton:hover{
-                                font-size: 15px;
-                                font-weight: bold;
-                            }""")
-                elif stato_tavolo == "prenotato":
-                    tavolo_button.setStyleSheet("""
-                            QPushButton{
-                                background-color: "#007fff";
-                                color: "white";
-                                text-align: center;
-                                border-radius: 6px;
-                                border: 3px solid lightgrey;
-                                font-family: Roboto;
-                            }QPushButton:checked{
-                                                    background-color: "lightblue";
                                                     color: "black";
                                                 }
                             QPushButton:hover{
@@ -120,9 +113,21 @@ class ContPiantina(object):
                                 font-size: 15px;
                                 font-weight: bold;
                             }""")
-
-
-
-
-
-
+                else:
+                    tavolo_button.setStyleSheet("""
+                                QPushButton{
+                                    background-color: "grey";
+                                    color: "black";
+                                    text-align: center;
+                                    border-radius: 6px;
+                                    border: 3px solid lightgrey;
+                                    font-family: Roboto;
+                                }
+                                QPushButton:checked{
+                                                        background-color: "lightgrey";
+                                                        color: "black";
+                                                    }
+                                QPushButton:hover{
+                                    font-size: 15px;
+                                    font-weight: bold;
+                                }""")
--- Code/GestionePrenotazioni/Model/gestore_prenotazioni.py
+++ Code/GestionePrenotazioni/Model/gestore_prenotazioni.py
@@ -14,60 +14,57 @@ class GestorePrenotazioni():
         self.lista_tavoli = self.gestore_ordini_tavolo.lista_tavoli
 
         self.ultimo_codice_prenotazione = 0
-        self.nome_file = "lista_prenotazioni.pickle"
+        self.nome_file_prenotazioni = "lista_prenotazioni.pickle"
+        self.nome_file_tavoli = "lista_tavoli.pickle"
         try:
-            self.carica_da_file(self.nome_file)
-            self.carica_tavoli()
+            self.carica_da_file(self.nome_file_prenotazioni, self.nome_file_tavoli)
+            # self.carica_tavoli()
         except FileNotFoundError:
             print("dati prenotazioni non trovati")
 
     def stampa_lista_tavoli(self):
-        print(self.lista_tavoli)
         for tavolo in self.lista_tavoli:
-            print("numeroT: " + str(tavolo.numero) + " posti: " + str(tavolo.posti_disponibili) + " stato: " + str(
-                tavolo.stato))
+            print(f"Tavolo {tavolo.numero}: Stato - {tavolo.stato}")
 
     def aggiungi_prenotazione(self, prenotazione):
         self.lista_prenotazioni.append(prenotazione)
         prenotazione.tavolo_assegnato.cambia_stato("prenotato")
         print(prenotazione.tavolo_assegnato.stato)
 
-        self.salva_dati(self.nome_file)
-        self.carica_da_file(self.nome_file)
-        self.salva_tavoli()
-        self.carica_tavoli()
+        self.salva_dati(self.nome_file_prenotazioni,self.nome_file_tavoli)
+        self.carica_da_file(self.nome_file_prenotazioni,self.nome_file_tavoli)
+        # self.salva_tavoli()
+        # self.carica_tavoli()
 
     def modifica_prenotazioni(self, codice_ricerca, new_tavolo, new_persone,
                               new_orario, new_data):
 
         pren_da_modificare: Prenotazione = self.ricerca_prenotazione_codice(codice_ricerca)
+        pren_da_modificare.tavolo_assegnato.cambia_stato("libero")
+
         for x in self.lista_prenotazioni:
             if int(pren_da_modificare.codice) == int(x.codice):
-                x.tavolo_assegnato.cambia_stato("libero")
                 x.tavolo_assegnato = new_tavolo
                 x.tavolo_assegnato.cambia_stato("prenotato")
                 x.n_persone = new_persone
                 x.orario = new_orario
                 x.data = new_data
 
-        self.salva_dati(self.nome_file)
-        self.carica_da_file(self.nome_file)
-        self.salva_tavoli()
-        self.carica_tavoli()
+        self.salva_dati(self.nome_file_prenotazioni, self.nome_file_tavoli)
+        self.carica_da_file(self.nome_file_prenotazioni, self.nome_file_tavoli)
+        # self.salva_tavoli()
+        # self.carica_tavoli()
+
     def elimina_prenotazione(self, codice):
         prenotazione_da_eliminare = self.ricerca_prenotazione_codice(codice)
-        prenotazione_da_eliminare.tavolo_assegnato.stato = "cambiato"
-        print(prenotazione_da_eliminare.tavolo_assegnato.numero)
-        print(prenotazione_da_eliminare.tavolo_assegnato.stato)
-
-        self.salva_tavoli()
-        self.carica_tavoli()
+        tavolo = self.ricerca_tavolo(prenotazione_da_eliminare.tavolo_assegnato.numero)
+        tavolo.cambia_stato("libero")
 
         self.lista_prenotazioni.remove(prenotazione_da_eliminare)
+        self.stampa_lista_tavoli()
 
-        self.salva_dati(self.nome_file)
-        self.carica_da_file(self.nome_file)
-
+        self.salva_dati(self.nome_file_prenotazioni, self.nome_file_tavoli)
+        self.carica_da_file(self.nome_file_prenotazioni, self.nome_file_tavoli)
 
     def ricerca_prenotazione_codice(self, codice):
         for prenotazione in self.lista_prenotazioni:
@@ -98,29 +95,40 @@ class GestorePrenotazioni():
 
     #############################################################
 
-    def salva_dati(self, nome_file):
-        dati = {
+    def salva_dati(self, nome_file_prenotazioni, nome_file_tavoli):
+        dati_prenotazioni = {
             'cod': self.ultimo_codice_prenotazione,
-            'lista': self.lista_prenotazioni,
+            'lista': self.lista_prenotazioni
         }
-        with open(nome_file, 'wb') as file:
-            pickle.dump(dati, file)
-        file.close()
 
-    def carica_da_file(self, nome_file):
-        with open(nome_file, 'rb') as file:
-            dati = pickle.load(file)
+        dati_tavoli = {
+            'tavoli': self.lista_tavoli
+        }
 
-        self.ultimo_codice_prenotazione = dati['cod']
-        self.lista_prenotazioni = dati['lista']
+        with open(nome_file_prenotazioni, 'wb') as file_prenotazioni:
+            pickle.dump(dati_prenotazioni, file_prenotazioni)
 
-    def salva_tavoli(self):
-        with open ("lista_tavoli.pickle", "wb") as file:
-            pickle.dump(self.lista_tavoli, file)
+        with open(nome_file_tavoli, 'wb') as file_tavoli:
+            pickle.dump(dati_tavoli, file_tavoli)
 
-    def carica_tavoli(self):
-        with open ("lista_tavoli.pickle", "rb") as file:
-            self.lista_tavoli = pickle.load(file)
+    def carica_da_file(self, nome_file_prenotazioni, nome_file_tavoli):
+        with open(nome_file_prenotazioni, 'rb') as file_prenotazioni:
+            dati_prenotazioni = pickle.load(file_prenotazioni)
+
+        with open(nome_file_tavoli, 'rb') as file_tavoli:
+            dati_tavoli = pickle.load(file_tavoli)
+
+        self.ultimo_codice_prenotazione = dati_prenotazioni['cod']
+        self.lista_prenotazioni = dati_prenotazioni['lista']
+        self.lista_tavoli = dati_tavoli['tavoli']
+
+    # def salva_tavoli(self):
+    #     with open ("lista_tavoli.pickle", "wb") as file:
+    #         pickle.dump(self.lista_tavoli, file)
+    #
+    # def carica_tavoli(self):
+    #     with open ("lista_tavoli.pickle", "rb") as file:
+    #         self.lista_tavoli = pickle.load(file)
 
     def get_info_prenotazioni(self):
         lista_prenotazioni = self.lista_prenotazioni.copy()
